014d6e3aba496982b00ab5d56137664b79db8086
sayanseksenbaev <sayan123serv@gmail.com>
2025-03-14 12:23:42 +0500

    Feat: authService has been done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,37 @@
-package server
+package main
+
+import (
+	"authService/internal/config"
+	"authService/internal/routes"
+	"authService/pkg/logging"
+	"gorm.io/driver/postgres"
+
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"log"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
+	db, err := gorm.Open(postgres.Open(cfg.DatabaseURL), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Ошибка подключения к базе данных:", err)
+	}
+
+	r := gin.Default()
+	r.Use(gin.LoggerWithFormatter(logging.CustomLogFormatter), gin.Recovery())
+	routes.SetupAuthRoutes(r, db, cfg)
+
+	err = r.Run(":" + cfg.Port)
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+		return
+	}
+
+}
